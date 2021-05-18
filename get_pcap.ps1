6cdbbdbdb8fbca96855c58f4090a842290a46b7c
Starke427 <28498535+Starke427@users.noreply.github.com>
2021-05-17 22:40:47 -0700

    Update get_pcap.ps1

--- get_pcap.ps1
+++ get_pcap.ps1
@@ -11,8 +11,8 @@ Expand-Archive -LiteralPath C:\Tshark.zip -DestinationPath C:\
 Remove-Item -Path C:\Tshark.zip -Force
 
 # Prompt for Tshark Interface
-Get-NetIPConfiguration | Select InterfaceAlias,IPv4Address | Sort-Object -Property InterfaceAlias # Get Interface names and IPs
-Start-Sleep 5
+$availableInterfaces = Get-NetIPConfiguration | Select InterfaceAlias,IPv4Address | Sort-Object -Property InterfaceAlias # Get Interface names and IPs
+echo $availableInterfaces
 $interface = Read-Host -Prompt "Please enter the InterfaceAlias you would like to capture: "
 
 # Run Tshark and Capture 50MB PCAP
